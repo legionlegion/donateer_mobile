667b134803e8d226f4177a180d48e054cbe35643
fwyyyin <isabellafunwenyin@gmail.com>
2022-06-01 00:39:25 +0800

    Updated favourites function

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -1,16 +1,145 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './organisation_details_screen.dart';
+import './tabs_screen.dart';
 
-class FavouritesScreen extends StatelessWidget {
+class FavouritesScreen extends StatefulWidget {
+  @override
+  State<FavouritesScreen> createState() => _FavouritesScreenState();
+}
+
+class _FavouritesScreenState extends State<FavouritesScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  late Future resultsLoaded;
+  List _allResults = [];
+  List _resultsList = [];
+  List _favourites = [];
+
+  @override
+  void initState() {
+    getStreamSnapshots();
+    getResultsList();
+    super.initState();
+  }
+
+  getStreamSnapshots() async {
+    var data =
+        await FirebaseFirestore.instance.collection('Organisations').get();
+    setState(() {
+      _allResults = data.docs;
+    });
+  }
+
+  getResultsList() async {
+    var fav = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user!.uid)
+        .get();
+    setState(() {
+      if (fav['favourites'] != null) {
+        _favourites = fav['favourites'];
+      } else {
+        _favourites = [];
+      }
+    });
+    var results = [];
+    for (var data in _allResults) {
+      if (_favourites.contains(data['name'])) {
+        results.add(data);
+      }
+    }
+    setState(() {
+      _resultsList = results;
+    });
+  }
+
+  updateFirestoreFavourites() async {
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user!.uid)
+        .update({'favourites': _favourites});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(22),
-        child: Column(children: [
+      padding: EdgeInsets.all(22),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           Text(
             'Favourites',
-            style: Theme.of(context).textTheme.headline1,
+            style: Theme.of(context).textTheme.headline1
           ),
-        ]));
+          Expanded(
+              child: ListView.builder(
+            itemCount: _resultsList.length,
+            itemBuilder: (ctx, index) => InkWell(
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  ctx,
+                  MaterialPageRoute(
+                    builder: (ctx) => OrganisationDetailsScreen(
+                        obj: _resultsList[index].data()),
+                  ),
+                  (route) => false,
+                );
+              },
+              child: Card(
+                margin: EdgeInsets.only(bottom: 16.0),
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ListTile(
+                      //leading: Icon(Icons.arrow_drop_down_circle),   // to add organisation logo
+                      trailing: IconButton(
+                        icon: _favourites.contains(_resultsList[index]['name'])
+                            ? Icon(
+                                Icons.favorite,
+                                color: Colors.red[400],
+                              )
+                            : const Icon(Icons.favorite_outline_rounded),
+                        onPressed: () async {
+                          if (!_favourites
+                              .contains(_resultsList[index]['name'])) {
+                            setState(() {
+                              _favourites.add(_resultsList[index]['name']);
+                            });
+                            updateFirestoreFavourites();
+                          } else if (_favourites
+                              .contains(_resultsList[index]['name'])) {
+                            setState(() {
+                              _favourites.remove(_resultsList[index]['name']);
+                              getResultsList();
+                            });
+                            updateFirestoreFavourites();
+                          }
+                        },
+                      ),
+                      title: Text(_resultsList[index]['name'],
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    Image.network(_resultsList[index]['imageUrl'],
+                        height: 145, fit: BoxFit.fitWidth),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        _resultsList[index]['description']
+                                .replaceAll("\\n", "\n")
+                                .substring(0, 90) +
+                            '...',
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/organisations_overview_screen.dart
+++ lib/screens/organisations_overview_screen.dart
@@ -6,28 +6,144 @@ import './organisation_details_screen.dart';
 import '../widgets/filter_dialog.dart';
 import './tabs_screen.dart';
 
-class OrganisationsOverviewScreen extends StatelessWidget {
-  User? user = FirebaseAuth.instance.currentUser;
+class OrganisationsOverviewScreen extends StatefulWidget {
   final filter;
 
   OrganisationsOverviewScreen({Key? key, this.filter}) : super(key: key);
 
   @override
+  State<OrganisationsOverviewScreen> createState() =>
+      _OrganisationsOverviewScreenState();
+}
+
+class _OrganisationsOverviewScreenState
+    extends State<OrganisationsOverviewScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  bool search = false;
+  TextEditingController _searchController = TextEditingController();
+  late Future resultsLoaded;
+  List _allResults = [];
+  List _resultsList = [];
+  List _favourites = [];
+
+  @override
+  void initState() {
+    getFavourites();
+    super.initState();
+    _searchController.addListener(searchResultsList);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(searchResultsList);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    getStreamSnapshots();
+  }
+
+  searchResultsList() {
+    var showResults = [];
+    var results = _allResults;
+    if (widget.filter != null) {
+      results = [];
+      for (var data in _allResults) {
+        if (data['categories']
+            .toSet()
+            .intersection(widget.filter['categories'].toSet())
+            .isNotEmpty) {
+          results.add(data);
+        }
+      }
+    }
+    if (_searchController.text != "") {
+      for (var data in results) {
+        var title = data['name'].toLowerCase();
+        if (title.contains(_searchController.text.toLowerCase())) {
+          showResults.add(data);
+        }
+      }
+    } else {
+      showResults = List.from(results);
+    }
+    setState(() {
+      _resultsList = showResults;
+    });
+  }
+
+  getStreamSnapshots() async {
+    var data =
+        await FirebaseFirestore.instance.collection('Organisations').get();
+    setState(() {
+      _allResults = data.docs;
+    });
+    searchResultsList();
+  }
+
+  getFavourites() async {
+    var data = await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user!.uid)
+        .get();
+    setState(() {
+      if (data['favourites'] != null) {
+        _favourites = data['favourites'];
+      } else {
+        _favourites = [];
+      }
+    });
+  }
+
+  updateFirestoreFavourites() async {
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user!.uid)
+        .update({'favourites': _favourites});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(14),
+      padding: EdgeInsets.all(22),
       child: Column(
         children: [
           Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(
-              'Get Involved',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {},
-            )
+            if (!search)
+              Text(
+                'Get Involved',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            if (!search)
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  setState(() {
+                    search = true;
+                  });
+                },
+              ),
+            if (search)
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(),
+                ),
+              ),
+            if (search)
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    search = false;
+                  });
+                },
+              ),
           ]),
+          const SizedBox(height: 5),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -67,74 +183,71 @@ class OrganisationsOverviewScreen extends StatelessWidget {
               const SizedBox(width: 10),
             ],
           ),
-          Flexible(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: (filter == null
-                  ? FirebaseFirestore.instance
-                      .collection('Organisations')
-                      .snapshots()
-                  : FirebaseFirestore.instance
-                      .collection('Organisations')
-                      .where('categories',
-                          arrayContainsAny: filter['categories'])
-                      .snapshots()),
-              builder: (ctx, AsyncSnapshot streamSnapshot) {
-                if (streamSnapshot.hasData) {
-                  var documents = streamSnapshot.data.docs;
-                  return ListView.builder(
-                    itemCount: documents.length,
-                    itemBuilder: (ctx, index) => InkWell(
-                      onTap: () {
-                        Navigator.pushAndRemoveUntil(
-                          ctx,
-                          MaterialPageRoute(
-                            builder: (ctx) => OrganisationDetailsScreen(
-                                obj: documents[index].data()),
-                          ),
-                          (route) => false,
-                        );
-                      },
-                      child: Card(
-                        margin: EdgeInsets.all(8),
-                        color: Theme.of(context).primaryColor,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            ListTile(
-                              //leading: Icon(Icons.arrow_drop_down_circle),   // to add organisation logo
-                              trailing: IconButton(
-                                icon:
-                                    const Icon(Icons.favorite_outline_rounded),
-                                onPressed: () {},
-                              ),
-                              title: Text(documents[index]['name'],
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                            ),
-                            Image.network(documents[index]['imageUrl'],
-                                height: 145, fit: BoxFit.fitWidth),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                documents[index]['description']
-                                        .replaceAll("\\n", "\n")
-                                        .substring(0, 90) +
-                                    '...',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                          ],
-                        ),
+          Expanded(
+              child: ListView.builder(
+            itemCount: _resultsList.length,
+            itemBuilder: (ctx, index) => InkWell(
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  ctx,
+                  MaterialPageRoute(
+                    builder: (ctx) => OrganisationDetailsScreen(
+                        obj: _resultsList[index].data()),
+                  ),
+                  (route) => false,
+                );
+              },
+              child: Card(
+                margin: EdgeInsets.only(bottom: 16.0),
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ListTile(
+                      //leading: Icon(Icons.arrow_drop_down_circle),   // to add organisation logo
+                      trailing: IconButton(
+                        icon: _favourites.contains(_resultsList[index]['name'])
+                            ? Icon(
+                                Icons.favorite,
+                                color: Colors.red[400],
+                              )
+                            : const Icon(Icons.favorite_outline_rounded),
+                        onPressed: () async {
+                          if (!_favourites
+                              .contains(_resultsList[index]['name'])) {
+                            setState(() {
+                              _favourites.add(_resultsList[index]['name']);
+                            });
+                            updateFirestoreFavourites();
+                          } else if (_favourites
+                              .contains(_resultsList[index]['name'])) {
+                            setState(() {
+                              _favourites.remove(_resultsList[index]['name']);
+                            });
+                            updateFirestoreFavourites();
+                          }
+                        },
+                      ),
+                      title: Text(_resultsList[index]['name'],
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    Image.network(_resultsList[index]['imageUrl'],
+                        height: 145, fit: BoxFit.fitWidth),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        _resultsList[index]['description']
+                                .replaceAll("\\n", "\n")
+                                .substring(0, 90) +
+                            '...',
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
                       ),
                     ),
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
+                  ],
+                ),
+              ),
             ),
-          )
+          )),
         ],
       ),
     );
